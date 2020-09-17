fe12587d23b72c356696837e4d11d8a49b2bfb05
Ai The Potato <37963198+AiThePotato@users.noreply.github.com>
2020-09-17 14:38:04 +0200

    Summary (required)

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h>
+ 
+int main() { 
+ 
+           /* I am C developer */ 
+
+           int a = 0, b = 1, c = 2, d = 3, e = 4;
+
+           a = b - c + d * e;
+ 
+           printf("Hello, world! \n"); 
+
+           printf("%d", a);
+
+           system("pause"); 
+               
+           return 0; 
+}
