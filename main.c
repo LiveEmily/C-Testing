e0acab26f23169359e8f92420df1572e66165684
Ai The Potato <37963198+AiThePotato@users.noreply.github.com>
2020-09-18 11:36:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int add(int a, int b, int sum);
-
-int i = 0;
  
 int main() { 
  
            /* I am C developer */ 
 
+           bool breakCode = true;
+
            char operator;
 
            int a, b, sum;
 
-           char yON;
+           char yOn;
 
-           while(i<10)
+           while(breakCode)
            {
                printf("What operation would you like to do? \n"); 
 
-               scanf(" %c", &operator);
+               scanf_s(" %c", &operator, 1);
 
                switch(operator)
                {
                     case '+':
                         printf("Please give me the first number to add together.\n"); 
 
-                        scanf(" %d", &a);
+                        scanf_s(" %d", &a, 1);
 
                         printf("Please give me the second number to add together.\n"); 
 
-                        scanf(" %d", &b);
+                        scanf_s(" %d", &b, 1);
 
                         printf("Your addition concluded in the number %d\n", add(a, b, sum));
 
@@ -39,11 +40,11 @@ int main() {
                     case '-':
                         printf("Please give me the first number to divide together.\n"); 
 
-                        scanf(" %d", &a);
+                        scanf_s(" %d", &a, 1);
 
                         printf("Please give me the second number to divide together.\n"); 
 
-                        scanf(" %d", &b);
+                        scanf_s(" %d", &b, 1);
 
                         printf("Your substraction concluded in the number %d\n", substract(a, b, sum));
 
@@ -52,11 +53,11 @@ int main() {
                     case '*':
                         printf("Please give me the first number to multiply together.\n"); 
 
-                        scanf(" %d", &a);
+                        scanf_s(" %d", &a, 1);
 
                         printf("Please give me the second number to multiply together.\n"); 
 
-                        scanf(" %d", &b);
+                        scanf_s(" %d", &b, 1);
 
                         printf("Your multiplication concluded in the number %d\n", multiply(a, b, sum));
 
@@ -65,11 +66,11 @@ int main() {
                     case '/':
                         printf("Please give me the first number to divide.\n"); 
 
-                        scanf(" %d", &a);
+                        scanf_s(" %d", &a, 1);
 
                         printf("Please give me the second number to divide.\n"); 
 
-                        scanf(" %d", &b);
+                        scanf_s(" %d", &b, 1);
 
                         printf("Your division concluded in the number %d\n", divide(a, b, sum));
 
@@ -78,11 +79,11 @@ int main() {
                     case '%':
                         printf("Please give me the first number to divide.\n"); 
 
-                        scanf(" %d", &a);
+                        scanf_s(" %d", &a, 1);
 
                         printf("Please give me the second number to divide.\n"); 
 
-                        scanf(" %d", &b);
+                        scanf_s(" %d", &b,1 );
 
                         printf("Your division concluded in the number %d as the remainder\n", remain(a, b, sum));
 
@@ -95,23 +96,26 @@ int main() {
 
                 printf("Would you like to do another math operation? y = yes, n = no\n");
 
-                yON = scanf(" %c", &yON);
+                scanf_s("%c", &yOn, 1);
 
-                if(yON == 'y')
+                if(yOn == 'y')
                 {
-                    return;
+                    breakCode = true;
                 }
                 else
                 {
-                    exit(0);
-                }
-                
+                    breakCode = false;
+                }              
 
                 system("cls");
            }
                
            return 0; 
 }
+
+int scanf_s(
+    const char *format []
+);
 
 int add(int a, int b, int sum)
 {
