8964824ee9ea5d1c8e5fb5ecefe172607e16cbe7
Ai The Potato <37963198+AiThePotato@users.noreply.github.com>
2020-09-18 14:21:15 +0200

    Memory.c/Memory.exe will crash your pc

--- memory.c
+++ memory.c
@@ -1,11 +1,11 @@
 #include <stdio.h>   
 #include <stdlib.h>
- 
+
 int main(void) 
 {
     while(1) 
     {
-        int *p1 = malloc(100000*sizeof *p1);
+        int *p1 = malloc(16384*sizeof *p1);
     };
 
     system("pause");
